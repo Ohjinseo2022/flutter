0a07edac67e93b811dbb5dcb1b4f0c5d7a0e6be2
Ohjinseo2022 <dhwlstj@gmail.com>
2024-05-17 10:29:14 +0900

    WebView 설정방법 및 appBar 의 간단한 사용 방법

--- web_view/lib/screen/home_screen.dart
+++ web_view/lib/screen/home_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final homeUrl = Uri.parse('https://www.naver.com/');
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  //WebViewController controller = WebViewController();
+  //controller.loadRequest(homeUrl);
+  // .. 은 함수실행된 결과값이아닌 함수가 실행되는 대상을 리턴해준다!
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // 상단 타이틀을 제공해줌
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: const Text(
+          '오진서 공부중',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        //앱바에 기능을 넣는 기능
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          )
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
   }
 }
