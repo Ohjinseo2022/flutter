85dfb13db334e356c25379a802886ef2a541afe5
Ohjinseo2022 <dhwlstj@gmail.com>
2024-05-26 21:09:22 +0900

    ElevatedButton 스타일 속성 및 사용 방법

--- buttons/lib/screen/home_screen.dart
+++ buttons/lib/screen/home_screen.dart
@@ -17,7 +17,33 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
+              //onPressed : null, ->> disable 처리가능
               onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                //배경 색깔
+                backgroundColor: Colors.red,
+                disabledBackgroundColor: Colors.grey,
+                //배경 위의 색깔
+                foregroundColor: Colors.white,
+                disabledForegroundColor: Colors.red,
+                // 그림자 색깔
+                shadowColor: Colors.green,
+                // 그림자 높이
+                elevation: 10,
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+                padding: EdgeInsets.all(20),
+                //테두리 디자인
+                side: BorderSide(
+                  color: Colors.deepPurpleAccent,
+                  width: 12,
+                ),
+                // minimumSize: Size(150, 50), // 최소 사이즈
+                // maximumSize: Size(100, 150), // 최대 사이즈
+                // fixedSize: Size(100, 150), //고정 사이즈
+              ),
               child: Text("Elevated Button"),
             ),
             OutlinedButton(
