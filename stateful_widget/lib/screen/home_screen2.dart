c2e42fab3c18c1bebe78875767dd7deb4f0293ad
Ohjinseo2022 <dhwlstj@gmail.com>
2024-06-04 14:54:45 +0900

    우분투 ssh 설정완료 추후 특정 포트만 접속가능하게 방화벽설정 별도 필요함

--- stateful_widget/lib/screen/home_screen2.dart
+++ stateful_widget/lib/screen/home_screen2.dart
@@ -25,7 +25,7 @@ class _HomeScreen2State extends State<HomeScreen2> {
                 }
                 setState(() {});
               },
-              child: Text("미니 pc 포트포워딩 예쩡")),
+              child: Text("우분투 ssh 외부접속 허용설정 완료")),
           SizedBox(
             height: 32,
           ),
