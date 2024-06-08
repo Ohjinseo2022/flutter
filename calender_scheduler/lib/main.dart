f56ba2cf368fe07f272542bcf59aa8d86c25dc57
Ohjinseo2022 <dhwlstj@gmail.com>
2024-06-08 20:30:20 +0900

    tableCalender locale 설정 방법

--- calender_scheduler/lib/main.dart
+++ calender_scheduler/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:calender_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting();
+
   runApp(MaterialApp(
     home: HomeScreen(),
   ));
--- calender_scheduler/lib/screen/home_screen.dart
+++ calender_scheduler/lib/screen/home_screen.dart
@@ -16,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
       //테이블 캘린더
       body: SafeArea(
         child: TableCalendar(
+          locale: 'ko_KR',
           //선택된 날짜가 아닌 내가 보여줘야할 날짜를 의미함
           focusedDay: DateTime(2024, 6, 1),
           //고를수 있는 날짜중 가장 첫번째 날짜
@@ -31,9 +32,9 @@ class _HomeScreenState extends State<HomeScreen> {
           },
           selectedDayPredicate: (DateTime day) {
             // 날짜가 선택된 날짜로 마킹해줄지 결정해주는 함수
-            print('--------');
-            print("선택한 날짜: " + day.toString());
-            print("비교할 날짜: " + this.selectedDay.toString());
+            // print('--------');
+            // print("선택한 날짜: " + day.toString());
+            // print("비교할 날짜: " + this.selectedDay.toString());
             // flutter: 선택한 날짜: 2024-07-06 00:00:00.000Z
             // flutter: 비교할 날짜: 2024-06-12 00:00:00.000
             // 타임존이다름
