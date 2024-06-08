79ba7d3676e5b84519174e572fa9e63d95af0cb4
Ohjinseo2022 <dhwlstj@gmail.com>
2024-06-08 20:47:50 +0900

    tableCalender 달력 스타일링 하기

--- calender_scheduler/lib/screen/home_screen.dart
+++ calender_scheduler/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calender_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -12,6 +13,17 @@ class _HomeScreenState extends State<HomeScreen> {
   DateTime? selectedDay;
   @override
   Widget build(BuildContext context) {
+    final defaultBosDecoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(6),
+      border: Border.all(
+        color: Colors.grey[200]!,
+        width: 1.0,
+      ),
+    );
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
     return Scaffold(
       //테이블 캘린더
       body: SafeArea(
@@ -23,6 +35,39 @@ class _HomeScreenState extends State<HomeScreen> {
           firstDay: DateTime(1800),
           //고를수 있는 날짜중 가장 마지막 날짜
           lastDay: DateTime(3000),
+          //헤더 스타일
+          headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              )),
+          calendarStyle: CalendarStyle(
+            //오늘이 몇일인지 확인
+            isTodayHighlighted: true,
+            //무언가 스타일이나 속성을 줄때는 모든 상황에 동일하게 적용 시켜 줘야 한다. -> 애니메이션 효과떄문에 스타일에 변경을 줄땐 한땀한땀 고쳐줘야함
+            defaultDecoration: defaultBosDecoration,
+            weekendDecoration: defaultBosDecoration,
+            selectedDecoration: defaultBosDecoration.copyWith(
+              border: Border.all(
+                color: primaryColor,
+                width: 1,
+              ),
+            ),
+            todayDecoration: defaultBosDecoration.copyWith(
+              color: primaryColor,
+            ),
+            outsideDecoration: defaultBosDecoration.copyWith(
+                border: Border.all(
+              color: Colors.transparent, //투명
+            )),
+            defaultTextStyle: defaultTextStyle,
+            weekendTextStyle: defaultTextStyle,
+            selectedTextStyle: defaultTextStyle.copyWith(
+              color: primaryColor,
+            ),
+          ),
           onDaySelected: (selectedDay, focusedDay) {
             print(selectedDay);
             print(focusedDay);
