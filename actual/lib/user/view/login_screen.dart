d793537ed66b16239bd883c1fabe59408c0bf8be
Ohjinseo2022 <dhwlstj@gmail.com>
2024-06-27 22:30:38 +0900

    이론강의 및 Dio 패키지 복습

--- actual/lib/user/view/login_screen.dart
+++ actual/lib/user/view/login_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual/common/component/custom_text_form_field.dart';
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,6 +12,15 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+    //현재 실행 환경 파악
+    // 애플에서 시뮬레이터를 쓸떄는 - 시뮬레이터와 네트워크 환경이 똑같음
+    // 안드로이드의 경우 애뮬레이터와 컴퓨터와 네트워크가 다름
+    const emulatorIp = '10.0.2.2:3000'; //안드 localhost
+    const simulatorIp = '127.0.0.1:3000';
+
+    final String ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       //외부 스크롤시 키보드 사라지게해야함
       child: SingleChildScrollView(
@@ -20,11 +33,11 @@ class LoginScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                _Title(),
+                const _Title(),
                 const SizedBox(
                   height: 16.0,
                 ),
-                _SubTitle(),
+                const _SubTitle(),
                 Image.asset(
                   'asset/img/misc/logo.png',
                   width: MediaQuery.of(context).size.width / 3 * 2,
@@ -49,11 +62,38 @@ class LoginScreen extends StatelessWidget {
                   style: ElevatedButton.styleFrom(
                       backgroundColor: PRIMARY_COLOR,
                       foregroundColor: Colors.white),
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID:비밀번호
+                    const rawString = 'test@codefactory.ai:testtest';
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+                    String token = stringToBase64.encode(rawString);
+                    final response = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+                    print(response.data);
+                  },
                   child: Text("로그인"),
                 ),
                 TextButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      // ID:비밀번호
+                      final String refreshToken =
+                          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTcxOTQ5NDY1NiwiZXhwIjoxNzE5NTgxMDU2fQ.QFhYmLh2Q6CM8UnzcWnuNEDqBs6qy0tqdKDkljw0BkM";
+                      final response = await dio.post(
+                        'http://$ip/auth/token',
+                        options: Options(
+                          headers: {
+                            'authorization': 'Bearer $refreshToken',
+                          },
+                        ),
+                      );
+                      print(response.data);
+                    },
                     style: TextButton.styleFrom(foregroundColor: PRIMARY_COLOR),
                     child: Text(
                       '회원가입',
