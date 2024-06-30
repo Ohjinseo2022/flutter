5857353060393734626b9cc8f50fc674580fa4f1
Ohjinseo2022 <dhwlstj@gmail.com>
2024-06-30 22:08:06 +0900

    AppBar 생성 및 BottomNavigationBar 추가

--- actual/lib/common/layout/default_layout.dart
+++ actual/lib/common/layout/default_layout.dart
@@ -3,14 +3,42 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
   DefaultLayout(
-      {super.key, required this.child, this.backgroundColor = Colors.white});
+      {super.key,
+      required this.child,
+      this.backgroundColor = Colors.white,
+      this.title,
+      this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        //튀어나옴 효과
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
