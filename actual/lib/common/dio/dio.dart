bfb0022ad7770e1578399979719a5aacf08c56cc
Ohjinseo2022 <dhwlstj@gmail.com>
2024-07-11 21:57:02 +0900

    CustomInterceptor onError 핸들러 추가 사용방법 마무리

--- actual/lib/common/dio/dio.dart
+++ actual/lib/common/dio/dio.dart
@@ -29,13 +29,50 @@ class CustomInterceptor extends Interceptor {
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     // TODO: implement onResponse
-    super.onResponse(response, handler);
+    print("[RES] [${response.requestOptions}] ${response.requestOptions.uri}");
+    return super.onResponse(response, handler);
   }
 
   // 3) 에어가 났을떄
   @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    // TODO: implement onError
-    super.onError(err, handler);
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    /**
+     * 401에러가 났을떄 (status code)
+     * 토큰을 재발급 받는 시도를하고 토근이 재발급되면
+     * 다시 새로운 토큰으로 요청한다.
+     * */
+    print("[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri}");
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    if (refreshToken == null) {
+      //리프레시 토큰 차제가 없다면 그대로 에러를 뱉어냄
+      return handler.reject(err);
+    }
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh =
+        err.requestOptions.path == '/auth/token'; //토큰을 새로 받급 받으려다가 에러난경우
+    //토큰을 재발급 하려는게 아니고 다른 api를 호출했을때 401(권한에러? ) 가 났다면
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+      try {
+        final res = await dio.post('http://$ip/auth/token',
+            options: Options(headers: {
+              'authorization': "Bearer $refreshToken",
+            }));
+        //토큰 변경 하기
+        final accessToken = res.data('accessToken');
+        final options = err.requestOptions;
+
+        options.headers.addAll({'authorization': 'Bearer $accessToken'});
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+        //요청 재전송
+        final response = await dio.fetch(options);
+        return handler.resolve(response);
+      } on DioException catch (e) {
+        //리프레시 토큰이 유효 하지 않다!
+        return handler.reject(e);
+      }
+    }
+
+    // return super.onError(err, handler);
   }
 }
