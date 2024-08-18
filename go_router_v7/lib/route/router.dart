23dad17c511d68a9ac0df7a5b5b8d0c4d30196cc
Ohjinseo2022 <dhwlstj@gmail.com>
2024-08-18 22:49:45 +0900

    go_router context.go / context.named / context.push / context.pop / 및 데이터 주고받기

--- go_router_v7/lib/route/router.dart
+++ go_router_v7/lib/route/router.dart
@@ -1,10 +1,49 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_v7/screens/1_basic_screen.dart';
+import 'package:go_router_v7/screens/2_named_screen.dart';
+import 'package:go_router_v7/screens/3_push_screen.dart';
+import 'package:go_router_v7/screens/4_pop_base_screen.dart';
+import 'package:go_router_v7/screens/5_pop_return_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://blog.codefactory.ai/ -> / -> path
 // https://blog.codefactory.ai/flutter -> /flutter
+// / -> home
+// /basic -> basicScreen
+// /basic/basic_two
+// /named
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => RootScreen()),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => RootScreen(),
+      //route 안에 중첩 가능함!
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) => BasicScreen(),
+        ),
+        GoRoute(
+          path: 'named',
+          name: 'named_screen',
+          builder: (context, state) => NamedScreen(),
+        ),
+        GoRoute(
+          path: 'push',
+          builder: (context, state) => PushScreen(),
+        ),
+        GoRoute(
+          path: 'pop',
+          builder: (context, state) => PopBaseScreen(),
+          routes: [
+            GoRoute(
+              path: 'return',
+              // /pop/return
+              builder: (context, state) => PopReturnScreen(),
+            )
+          ],
+        ),
+      ],
+    ),
   ],
 );
--- go_router_v7/lib/screens/root_screen.dart
+++ go_router_v7/lib/screens/root_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:go_router_v7/layout/default_layout.dart';
 
 class RootScreen extends StatelessWidget {
@@ -8,7 +9,32 @@ class RootScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
         body: ListView(
-      children: [],
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            context.go('/basic');
+          },
+          child: Text('Go_Basic'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            context.goNamed('named_screen');
+          },
+          child: Text('Go_Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            context.go('/push');
+          },
+          child: Text('Go_Push'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            context.go('/pop');
+          },
+          child: Text('Go_Pop'),
+        ),
+      ],
     ));
   }
 }
